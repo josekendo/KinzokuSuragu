52718cf17c583e517daffe56f53c06937577bd02
josekendo <josevicenteantoncoy@hotmail.com>
2018-04-05 11:53:22 -0700

    Clase Fachada
    exacto facade no hay mucho que decir, adaptara todas las llamadas que se hagan al sfml para desacoplar el motor grafico del juego en si.

---
+++ Motor2D.cpp
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Motor2D.cpp
+ * Author: fv
+ * 
+ * Created on April 5, 2018, 10:08 AM
+ */
+
+#include "Motor2D.hpp"
+
+Motor2D::Motor2D() {
+}
+
+Motor2D::Motor2D(const Motor2D& orig) {
+}
+
+Motor2D::~Motor2D() {
+}
+
---
+++ Motor2D.hpp
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Motor2D.hpp
+ * Author: fv
+ *
+ * Created on April 5, 2018, 10:08 AM
+ */
+
+#ifndef MOTOR2D_HPP
+#define MOTOR2D_HPP
+
+class Motor2D {
+public:
+    Motor2D();
+    Motor2D(const Motor2D& orig);
+    virtual ~Motor2D();
+private:
+
+};
+
+#endif /* MOTOR2D_HPP */
+
