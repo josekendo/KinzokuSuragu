3c720a70f8787b04eb1567029110b97535ab574e
josekendo <josevicenteantoncoy@hotmail.com>
2018-04-05 11:44:17 -0700

    Clase coordenadas vacia
    Coordenadas tendra una clase de la que dependera jugador y enemigos, en ella habra una que sea coordenada interpolada x y para facilitar la vida a todos

---
+++ Coordenadas.cpp
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Coordenadas.cpp
+ * Author: fv
+ * 
+ * Created on April 5, 2018, 11:42 AM
+ */
+
+#include "Coordenadas.hpp"
+
+Coordenadas::Coordenadas() {
+}
+
+Coordenadas::Coordenadas(const Coordenadas& orig) {
+}
+
+Coordenadas::~Coordenadas() {
+}
+
---
+++ Coordenadas.hpp
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Coordenadas.hpp
+ * Author: fv
+ *
+ * Created on April 5, 2018, 11:42 AM
+ */
+
+#ifndef COORDENADAS_HPP
+#define COORDENADAS_HPP
+
+class Coordenadas {
+public:
+    Coordenadas();
+    Coordenadas(const Coordenadas& orig);
+    virtual ~Coordenadas();
+private:
+
+};
+
+#endif /* COORDENADAS_HPP */
+
