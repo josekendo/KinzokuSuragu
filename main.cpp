697a36d215b58e3eb953be8faa3bc4e6255c4a02
josekendo <josevicenteantoncoy@hotmail.com>
2018-04-09 10:16:29 -0700

    main actualizado (lo dejo listo para cargar nivel)

--- main.cpp
+++ main.cpp
@@ -1,23 +1,12 @@
 //Librerias
-#include <iostream>
-#include <SFML/Graphics.hpp>
-#include <cmath>
-#include <sstream>
 #include "Juego.hpp"
-#include "Motor2D.hpp"
 #include "Nivel.hpp"
-#include "Coordenadas.hpp"
-#include "Jugadores.hpp"
-#include "Enemigos.hpp"
 
 int main()
 {
     //adaptacion nuevo main
         Juego *juego = Juego::getInstance();//clase global
-        Motor2D *motor2D = Motor2D::getInstance();//clase global
         Nivel *nivel = Nivel::getInstance();//clase global
-        Jugadores jugador();
-        Enemigos enemigo();
     //fin adaptacion nuevo main bloque externo
         juego->cargarNivel(2);
         nivel->juegoMultijugador(0,0);
