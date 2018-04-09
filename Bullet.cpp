80c76cb3289373247bf676c64d6179331b795485
josekendo <josevicenteantoncoy@hotmail.com>
2018-04-09 11:43:06 -0700

    clase vacia para proyectiles

---
+++ Bullet.cpp
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Bullet.cpp
+ * Author: fv
+ * 
+ * Created on April 9, 2018, 10:09 AM
+ */
+
+#include "Bullet.hpp"
+
+Bullet::Bullet() {
+}
+
+Bullet::Bullet(const Bullet& orig) {
+}
+
+Bullet::~Bullet() {
+}
+
---
+++ Bullet.hpp
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Bullet.hpp
+ * Author: fv
+ *
+ * Created on April 9, 2018, 10:09 AM
+ */
+
+#ifndef BULLET_HPP
+#define BULLET_HPP
+
+class Bullet {
+public:
+    Bullet();
+    Bullet(const Bullet& orig);
+    virtual ~Bullet();
+private:
+
+};
+
+#endif /* BULLET_HPP */
+
