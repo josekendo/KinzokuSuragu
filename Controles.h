3f925daa3fe9330a5ade22e89d07ca46cbaf1c09
josekendo <josevicenteantoncoy@hotmail.com>
2018-04-23 09:53:25 -0700

    Controles 2.0
    Permite multiples teclas.

--- Controles.h
+++ Controles.h
@@ -41,6 +41,27 @@ public:
     sf::Keyboard::Key moveDefensa();
     sf::Keyboard::Key moveSalir();
     sf::Keyboard::Key moveAtras();
+    /*control dos punto cero*/
+    void pu(int play){if(play == 1){up = true;}else{up2=true;}};//se pulsa arriba
+    void pd(int play){if(play == 1){down = true;}else{down2=true;}};//se pulsa abajo
+    void pr(int play){if(play == 1){right = true;}else{right2=true;}};//se pulsa derecha
+    void pl(int play){if(play == 1){left = true;}else{left2=true;}};//se pulsa izquierda
+    void pa(int play){if(play == 1){ataque = true;}else{ataque2=true;}};//se pulsa ataque
+    void pf(int play){if(play == 1){defensa = true;}else{defensa2=true;}};//se pulsa defensa
+    void du(int play){if(play == 1){up = false;}else{up2=false;}};//se despulsa arriba
+    void dd(int play){if(play == 1){down = false;}else{down2=false;}};//se despulsa abajo
+    void dr(int play){if(play == 1){right = false;}else{right2=false;}};//se despulsa derecha
+    void dl(int play){if(play == 1){left = false;}else{left2=false;}};//se despulsa izquierda
+    void da(int play){if(play == 1){ataque = false;}else{ataque2=false;}};//se despulsa ataque
+    void df(int play){if(play == 1){defensa = true;}else{defensa2=true;}};//se despulsa defensa
+    bool getu(int play){if(play == 1){return up;}else{return up2;}};
+    bool getd(int play){if(play == 1){return down;}else{return down2;}};
+    bool getr(int play){if(play == 1){return right;}else{return right2;}};
+    bool getl(int play){if(play == 1){return left;}else{return left2;}};
+    bool geta(int play){if(play == 1){return ataque;}else{return ataque2;}};
+    bool getf(int play){if(play == 1){return defensa;}else{return defensa2;}};
+    
+    bool up=false,down=false,right=false,left=false,ataque=false,defensa=false,up2=false,down2=false,right2=false,left2=false,ataque2=false,defensa2=false;
 };
 
 #endif /* CONTROLES_H */
