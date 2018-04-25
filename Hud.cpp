abc963b3b57bbff2563bae8521df60b65cf9f859
josekendo <josevicenteantoncoy@hotmail.com>
2018-04-25 07:42:23 -0700

    Creacion de clase HUD
    Sera llamado desde personaje para cambiar sus valores que se actualizaran en el proximo frame.
    
    Es una clase Global.

---
+++ Hud.cpp
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Hud.cpp
+ * Author: fv
+ * 
+ * Created on April 25, 2018, 7:38 AM
+ */
+
+#include "Hud.hpp"
+
+Hud::Hud() {
+}
+
+Hud::Hud(const Hud& orig) {
+}
+
+Hud::~Hud() {
+}
+
---
+++ Hud.hpp
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Hud.hpp
+ * Author: fv
+ *
+ * Created on April 25, 2018, 7:38 AM
+ */
+
+#ifndef HUD_HPP
+#define HUD_HPP
+
+class Hud {
+public:
+    Hud();
+    Hud(const Hud& orig);
+    virtual ~Hud();
+private:
+
+};
+
+#endif /* HUD_HPP */
+
